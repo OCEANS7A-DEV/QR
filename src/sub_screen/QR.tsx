f00e3d467bf2f997b270741fe1aa45da7b4c9054
aristos <storm1legendaryhero@gmail.com>
2024-10-09 12:12:26 +0900

    QR変更

--- src/sub_screen/QR.tsx
+++ src/sub_screen/QR.tsx
@@ -1,32 +1,60 @@
 import React, { useRef, useEffect, useState } from 'react';
+import Webcam from 'react-webcam';
+import jsQR from 'jsqr';
+
 const QR = () => {
-
+  const webcamRef = useRef<Webcam>(null);
+  const [data, setData] = useState<string | null>(null);
+  const videoConstraints = {
+    facingMode: { exact: 'environment' },
+  };
   useEffect(() => {
-    const handleMessage = (event) => {
-      // event.originで安全に発信元を確認することができます
-      if (event.origin !== 'https://script.google.com/macros/s/AKfycbwA4v7uWv585N8p3sUifQI1bKnLTSLQg79raDAVDgr1UEeAtgBdHQ7VxbCljONBwXTk2Q/exec') return;
+    const interval = setInterval(() => {
+      if (webcamRef.current) {
+        const imageSrc = webcamRef.current.getScreenshot();
+        if (imageSrc) {
+          const image = new Image();
+          image.src = imageSrc;
+          image.onload = () => {
+            const canvas = document.createElement('canvas');
+            canvas.width = image.width;
+            canvas.height = image.height;
+            const context = canvas.getContext('2d');
+            if (context) {
+              context.drawImage(image, 0, 0, image.width, image.height);
+              const imageData = context.getImageData(0, 0, image.width, image.height);
+              const code = jsQR(imageData.data, imageData.width, imageData.height);
+              if (code) {
+                setData(code.data);
+                clearInterval(interval);
+              }
+            }
+          };
+        }
+      }
+    }, 1000);
 
-      console.log('Received message from iframe:', event.data);
-    };
-
-    window.addEventListener('message', handleMessage);
-
-    return () => {
-      window.removeEventListener('message', handleMessage);
-    };
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div>
-      <iframe
-        src="https://script.google.com/macros/s/AKfycbwA4v7uWv585N8p3sUifQI1bKnLTSLQg79raDAVDgr1UEeAtgBdHQ7VxbCljONBwXTk2Q/exec"
-        title="External Page"
-        width="100%"
-        height="500px"
-        style={{ border: 'none' }}
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/png"
+        width={500}
+        videoConstraints={videoConstraints}
       />
+      <div>
+        {data ? (
+          <p>QRコードを読み取りました: {data}</p>
+        ) : (
+          <p>QRコードをスキャンしてください...</p>
+        )}
+      </div>
+      <button type="button" onClick={() => window.close()}>閉じる</button>
     </div>
   );
 };
-
 export default QR;
