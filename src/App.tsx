b19f6e611a7ac139c4fab020160f16fba155b696
aristos <storm1legendaryhero@gmail.com>
2024-10-09 13:10:44 +0900

    t

--- src/App.tsx
+++ src/App.tsx
@@ -12,38 +12,23 @@ import QR from './sub_screen/QR.tsx';
 export default function App() {
   const [currentPage, setCurrentPage] = useState('ReceivingPage');
   const nodeRef = useRef(null);
-  const [isMobile, setIsMobile] = useState(false);
   useEffect(() => {
     localStorageSet();
     localStoreSet();
   },[]);
 
-  useEffect(() => {
-    const userAgent = navigator.userAgent.toLowerCase();
-    if (/android|iphone|ipad|ipod/.test(userAgent)) {
-      setIsMobile(true);
-    }
-  }, []);
 
 
   const getPageComponent = (page: string) => {
-
-    if (isMobile) {
-      switch (page) {
-        case 'QRPage':
-          return <QR />;
-        default:
-          return null;
-      }
-    } else {
-      switch (page) {
-        case 'ReceivingPage':
-          return <ReceivingPage/>
-        case 'HQPage':
-          return <HQPage/>;
-        default:
-          return null;
-      }
+    switch (page) {
+      case 'ReceivingPage':
+        return <ReceivingPage/>
+      case 'HQPage':
+        return <HQPage/>;
+      case 'QRPage':
+        return <QR />;
+      default:
+        return null;
     }
   };
 
