859225973cb61a2b5c6115378cee54b48c5be227
aristos <storm1legendaryhero@gmail.com>
2024-10-09 13:30:59 +0900

    set

--- src/sub_screen/banner.tsx
+++ src/sub_screen/banner.tsx
@@ -13,33 +13,6 @@ export default function TopBanner({ setCurrentPage }: SettingProps) {
   };
   const [scannedData, setscannedData] = useState<string>('');
 
-  const handleQRClick = () => {
-    // QR読み取りページを新しいタブで開く
-    window.open(
-      'https://script.google.com/macros/s/AKfycbwA4v7uWv585N8p3sUifQI1bKnLTSLQg79raDAVDgr1UEeAtgBdHQ7VxbCljONBwXTk2Q/exec',
-      'QRReaderWindow',
-      'width=800,height=600'
-    );
-  };
-
-  // データを受信するためのメッセージイベントのリスナーを設定
-  React.useEffect(() => {
-    const handleMessage = (event: MessageEvent) => {
-      // 外部サイトからのデータを受信
-      if (event.origin === 'https://script.google.com') {
-        const scannedData = event.data;
-        console.log('取得したデータ:', scannedData);
-        setscannedData(scannedData);
-        // 必要な処理をここで実行
-      }
-    };
-
-    window.addEventListener('message', handleMessage);
-
-    return () => {
-      window.removeEventListener('message', handleMessage);
-    };
-  }, []);
 
   return (
     <div>
@@ -50,7 +23,7 @@ export default function TopBanner({ setCurrentPage }: SettingProps) {
         <a className="buttonUnderline" id="main_back" type="button" onClick={() => clickpage('HQPage')}>
           納品書作成
         </a>
-        <a className="buttonUnderline" id="main_back" type="button" onClick={() => clickpage('QR')}>
+        <a className="buttonUnderline" id="main_back" type="button" onClick={() => clickpage('QRPage')}>
           QR
         </a>
         <div>{scannedData}</div>
